425f210b9d73c341542378c949eff5d7dd7553a2
Joaquin B <joaquin.bachler@gmail.com>
2024-11-28 09:51:58 -0300

    fondo

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gradient-to-r from-indigo-500 ...">
       <header className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex items-center justify-between">
