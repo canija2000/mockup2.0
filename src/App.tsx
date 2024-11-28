63b5c700a2a5ad49c7d260e6d1460c071b315891
Joaquin B <joaquin.bachler@gmail.com>
2024-11-28 09:58:09 -0300

    fheadero

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-indigo-200 ...">
-      <header className="bg-white shadow-sm">
+      <header className="bg-indigo-200 shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-2">
